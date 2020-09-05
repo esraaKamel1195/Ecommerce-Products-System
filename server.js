61c0125da13ef92bfffe99be80b3674a3d9cc41e
Esraa Kamel <esraa.kamel1811@gmail.com>
2020-09-05 16:05:11 +0200

    Change direction in server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const serveStatic = require("serve-static");
 
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/crud-with-angular8"));
 
 app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/crud-with-angular8/index.html"));
 });
 // Start the app by listening on the default
 // Heroku port
