8d0566f4e63f27ce454619c94e6e98651ed3af84
Esraa Kamel <esraa.kamel1811@gmail.com>
2020-09-05 14:36:56 +0200

    Restructure the project to deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const serveStatic = require("serve-static");
+
+
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8000);
