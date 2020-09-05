a71c5937858e35ba59cb2807215e2d4fc0f427cf
Esraa Kamel <esraa.kamel1811@gmail.com>
2020-09-06 00:15:26 +0200

    Set API environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  API_URL: 'http://localhost:5000/',
+  API_URL: 'https://nodejs-api-for-crud.herokuapp.com/',
   production: false
   
 };
