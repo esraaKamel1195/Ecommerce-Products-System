9caf641459858a3f3aeccd0720b724322457abab
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-10-31 19:42:16 +0200

    Add Auth module component

---
+++ src/app/Authentication/auth.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AuthComponent } from '../Authentication/auth/auth.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ChangedPasswordComponent } from './changed-password/changed-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+@NgModule({
+  declarations:[
+    AuthComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetPasswordComponent,
+    ChangedPasswordComponent,
+    ResetPasswordComponent
+  ],
+  imports:[
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: []
+})
+export class AuthModule {}
---
+++ src/app/Authentication/changed-password/changed-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-changed-password',
+  templateUrl: './changed-password.component.html',
+  styleUrl: './changed-password.component.css'
+})
+export class ChangedPasswordComponent {
+
+}
